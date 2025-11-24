824acc937c9e2f9cb4f77b8216737f7d2a5e6176
bjeames <bjeames@usfca.edu>
2025-11-24 08:37:51 -0800

    solved 238 again with notes

--- 238.py
+++ 238.py
@@ -1,14 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # create a list to store the result
         res = [1] * len(nums)
-        current_prod = 1
-        current_suffix = 1
-
-        #left to right:
+        # track the current left product
+        l = 1
+        # track the current right product
+        r = 1
+        # iterate from left to right, store product of every num to the left of current index
+        # start at index = 1 so you can use i - 1
         for i in range(1, len(nums)):
-            res[i] = nums[i-1] * current_prod
-            current_prod = res[i]
-        
+            res[i] = nums[i-1] * l
+            l = res[i]
+        # iterate from right to left, multiply result by the right product
         for i in range(len(nums)-1, -1, -1):
-            res[i] = res[i] * current_suffix
-            current_suffix *= nums[i]
+            res[i] = res[i] * r
+            r = r * nums[i]
+        # return result
+        return res
