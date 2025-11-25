9ec8a7b97422f7ee949e6ab80a242fc3e34e1e75
bjeames <bjeames@usfca.edu>
2025-11-24 19:56:23 -0800

    solved 392 again, this time with actual two pointers

--- 392.py
+++ 392.py
@@ -1,14 +1,23 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        # if s doesn't exist, return true, would want to clarify this. If s is an
+        #empty string, could it be a valid subsequence?
         if not s:
             return True
+        # if t doesn't exist, then it is impossible for something to be a substring of t
         if not t:
             return False
-        s_list = list(s)
+        # make s pointer
+        s_pointer = 0
+        # id how long s is, if s pointer makes it to that length, return true
+        target = len(s)
+
+        #iterate over each item of t, if t[i] = s[pointer], move both pointers forward, else
+        # only move t pointer forward.
         for i in range(len(t)):
-            if t[i] == s_list[0]:
-                s_list.pop(0)
-                if len(s_list) == 0:
-                    return True
-                    
+            if t[i] == s[s_pointer]:
+                s_pointer += 1
+            if s_pointer == target:
+                return True
+            
         return False
