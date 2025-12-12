f941e5db31f51a1cd3c638c8b9776dfbe59a1a0e
bjeames <bjeames@usfca.edu>
2025-12-12 15:10:10 -0800

    solved 104 and 226

--- 104.py
+++ 104.py
@@ -13,3 +13,20 @@ class Solution:
         right = self.maxDepth(root.right)
 
         return 1 + max(left, right)
+    
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        if not root.left and not root.right:
+            return 1
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
