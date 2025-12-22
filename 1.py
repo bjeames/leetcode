01624659167620625ddf1e1841d7e945f4101ca6
bjeames <bjeames@usfca.edu>
2025-12-22 10:27:50 -0800

    solved 1

---
+++ 1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        res = [-1, -1]
+
+        for i in range(len(nums)):
+            comp = target-nums[i]
+            if comp in hash_map:
+                return [i, hash_map.get(comp)]
+            hash_map[nums[i]] = iclass Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        res = [-1, -1]
+
+        for i in range(len(nums)):
+            comp = target-nums[i]
+            if comp in hash_map:
+                return [i, hash_map.get(comp)]
+            hash_map[nums[i]] = iclass Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        res = [-1, -1]
+
+        for i in range(len(nums)):
+            comp = target-nums[i]
+            if comp in hash_map:
+                return [i, hash_map.get(comp)]
+            hash_map[nums[i]] = iclass Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        res = [-1, -1]
+
+        for i in range(len(nums)):
+            comp = target-nums[i]
+            if comp in hash_map:
+                return [i, hash_map.get(comp)]
+            hash_map[nums[i]] = iclass Solution:
+            
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        res = [-1, -1]
+
+        for i in range(len(nums)):
+            comp = target-nums[i]
+            if comp in hash_map:
+                return [i, hash_map.get(comp)]
+            hash_map[nums[i]] = i
