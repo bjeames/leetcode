b9815712b051e0be9246b9475972303eab2c09df
bjeames <bjeames@usfca.edu>
2026-01-09 13:45:32 -0800

    solved 323

--- 323.py
+++ 323.py
@@ -20,5 +20,33 @@ class Solution:
                 dfs(i)
 
         return counter
+    
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = {i:[] for i in range(n)}
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visited = set()
+
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for nei in adj[node]:
+                dfs(nei)
+            return
+
+        counter = 0
+        for i in range(n):
+            if i not in visited:
+                counter+=1
+            dfs(i)
+
+        return counter
+        
+
+
         
 
