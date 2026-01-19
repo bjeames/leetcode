5eaafc67572c21ab45afa6daaa45b2fdcc0de29e
bjeames <bjeames@usfca.edu>
2026-01-18 16:08:34 -0800

    solved 647

---
+++ 647.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        counter = 0
+        n = len(s)
+
+        for i in range(n):
+            l, r = i, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                counter+=1
+                l-=1
+                r+=1
+            l,r = i, i+1
+            while l >= 0 and r < n and s[l] == s[r]:
+                counter+=1
+                l-=1
+                r+=1
+        return counter
