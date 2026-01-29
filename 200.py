8f157d2804b99cde0a252d70aabfa3e44b6a1c3b
bjeames <bjeames@usfca.edu>
2026-01-29 13:09:13 -0800

    solved 200

--- 200.py
+++ 200.py
@@ -29,3 +29,32 @@ class Solution:
         
         return islands
 
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        seen = set()
+        counter = 0
+
+        def dfs(r,c):
+            if (
+                min(r,c) < 0 or
+                r >= rows or c >= cols or
+                (r,c) in seen or
+                grid[r][c] == '0'
+                ): return
+            else:
+                seen.add((r,c))
+                dfs(r+1,c)
+                dfs(r-1,c)
+                dfs(r,c+1)
+                dfs(r,c-1)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in seen:
+                    dfs(r,c)
+                    counter +=1
+        return counter
+
