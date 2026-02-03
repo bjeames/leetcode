20ffad70502988a2945da00aca12f8f4f8ad0595
bjeames <bjeames@usfca.edu>
2026-02-02 16:10:49 -0800

    solved 238

--- 238.py
+++ 238.py
@@ -17,3 +17,36 @@ class Solution:
             r = r * nums[i]
         # return result
         return res
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+
+        return res
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0] * n
+
+        prefix = 1
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(n-1, -1, -1):
+            res[i]*=postfix
+            postfix*=nums[i]
+        
+        return res
