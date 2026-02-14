a7acf545aeb39681ef68adc6c84a90562a5ea331
bjeames <bjeames@usfca.edu>
2026-02-14 09:20:16 -0800

    solved 199

--- 199.py
+++ 199.py
@@ -26,5 +26,36 @@ class Solution:
             
             res.append(furthest_right)
         return res
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        queue = deque([root])
+        res = []
+        while queue:
+            cur_lvl = []
+            for i in range(len(queue)):
+                cur_node = queue.popleft()
+                if cur_node.left:
+                    queue.append(cur_node.left)
+                if cur_node.right:
+                    queue.append(cur_node.right)
+                cur_lvl.append(cur_node.val)
+
+            res.append(cur_lvl[-1])
+        return res
+
+
+
+        
 
         
